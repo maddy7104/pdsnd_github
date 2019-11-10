195d0f0e609b72f2bcd34404d9b8c1cd5dac3ff6
Madhava Venkatesh <mv4437@gmail.com>
2019-11-10 13:49:48 -0500

    Added line comment about data dictionary and lists creation

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-
+#Creating city data dictionary and valid months and days lists
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
